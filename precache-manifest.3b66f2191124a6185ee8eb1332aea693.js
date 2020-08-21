e0beada23d1c3cb35f07469454b4f2c17c2d6b11
Aramis Diaz <aramisdiaz@mail.usf.edu>
2020-08-20 17:57:05 -0700

    Updates

---
+++ precache-manifest.3b66f2191124a6185ee8eb1332aea693.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6726261c2b22e10255041b144529201",
+    "url": "/ClickerTale/index.html"
+  },
+  {
+    "revision": "9766f98782ef924260a1",
+    "url": "/ClickerTale/static/css/main.a1576cbc.chunk.css"
+  },
+  {
+    "revision": "ddc2ef7b358bbc26ac40",
+    "url": "/ClickerTale/static/js/2.aa000a34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClickerTale/static/js/2.aa000a34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9766f98782ef924260a1",
+    "url": "/ClickerTale/static/js/main.613457f7.chunk.js"
+  },
+  {
+    "revision": "90344b35116453f7b362",
+    "url": "/ClickerTale/static/js/runtime-main.1c019c9c.js"
+  }
+]);
--- src/components/Banner/Banner.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./Banner.css";
-
-const Banner = props => <h1 className="banner">{props.children}</h1>;
-
-export default Banner;
